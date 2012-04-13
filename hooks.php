0b1ee1f5f37bd0b11baae7c5fe672ad1d7c28510
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-14 01:17:12 +0200

    Initial version of my work-around for the quite limited email service hook
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ hooks.php
@@ -0,0 +1,174 @@
+<?
+
+if (!defined('HTTP_METH_PATCH'))
+	define('HTTP_METH_PATCH', http_request_method_register('PATCH'));
+
+$url = 'https://api.github.com/';
+
+$user = isset($_POST['user']) ? $_POST['user'] : '';
+$pwd = isset($_POST['pwd']) ? $_POST['pwd'] : '';
+$repository = isset($_POST['repository']) ? $_POST['repository'] : '';
+
+if ($repository != '' && !strpos($repository, '/'))
+	$repository = $user . '/' . $repository;
+
+print('GitHub credentials:');
+print('<dl>');
+print('<form id="credentials" method="POST">');
+print('<dt><strong>User</strong></dt><dd><input type="text" name="user" value="' . $user . '" /></dd>');
+print('<dt><strong>Password</strong></dt><dd><input type="password" name="pwd" value="' . $pwd . '" /></dd>');
+print('<dt><strong>Repository</strong></dt><dd><input type="text" name="repository" value="' . $repository . '" /></dd>');
+print('</dl>');
+print('<input type="submit" value="Update" />');
+print('</form><br />');
+
+if ($pwd == '')
+	exit();
+
+print('<hr /><br />');
+
+function send_request($method, $command, $body) {
+	global $url, $user, $pwd, $repository;
+	$response = http_request($method, $url . 'repos/' . $repository . '/' . $command,
+		$body, array('httpauth' => $user . ':' . $pwd));
+	$header_end = strpos($response, "\r\n\r\n");
+	$headers = substr($response, 0, $header_end + 2);
+	$body = substr($response, $header_end + 4);
+	return json_decode($body);
+}
+
+function form_header($id, $action, $id = false) {
+	global $user, $pwd, $repository;
+	print('<form id="' . $id . '" method="POST">');
+	print('<input type="hidden" name="repository" value="' . $repository . '" />');
+	print('<input type="hidden" name="user" value="' . $user . '" />');
+	print('<input type="hidden" name="pwd" value="' . $pwd . '" />');
+	print('<input type="hidden" name="action" value="' . $action . '" />');
+	if ($id)
+		print('<input type="hidden" name="id" value="' . $id . '" />');
+}
+
+function list_hooks() {
+	$result = send_request(HTTP_METH_GET, 'hooks', '');
+	print('Hooks:<br />');
+	print('<table border="1">');
+	print('<tr><th>Id</th><th>Active</th><th>Event</th><th>URL</th><th>Edit</th><th>Delete</th></tr>');
+	foreach ($result as $hook) {
+		$id = $hook->id;
+		print('<tr>');
+		print('<td>' . $id . '</td>');
+		print('<td>' . ($hook->active ? '' : 'not ') . 'active</td>');
+		print('<td>' . join(' ', $hook->events) . '</td>');
+		print('<td><a href="' . $hook->config->url. '">' . htmlentities($hook->config->url) . '</a></td>');
+		print('<td>');
+		form_header('hook-' . $id, 'edit', $id);
+		print('<input type="submit" value="Edit" />');
+		print('</form>');
+		print('</td>');
+		print('<td>');
+		form_header('delete-hook-' . $id, 'delete', $id);
+		print('<input type="submit" value="Delete" />');
+		print('</form>');
+		print('</td>');
+		print('</tr>');
+	}
+	print('</table>');
+	form_header('add-new-hook', 'new');
+	print('<input type="submit" value="Add" />');
+	print('</form>');
+}
+
+function edit_hook($id) {
+	global $user, $pwd, $repository;
+	if ($id)
+		$hook = send_request(HTTP_METH_GET, 'hooks/' . $id, '');
+	else
+		$hook = (object)array('active' => '1', 'events' => array('pull_request'), 'secret' => '');
+	print('Edit hook ' . ($id ? $id : '') . '<br />');
+	form_header($id ? 'edit-hook-' . $id : 'add-hook', $id ? 'patch' : 'add', $id);
+	print('<dl>');
+	if ($id) {
+		print('<dt><strong>Created at</strong></dt><dd>' . $hook->created_at . '</dd>');
+		print('<dt><strong>Updated at</strong></dt><dd>' . $hook->updated_at . '</dd>');
+	}
+	print('<dt><strong>Active</strong></dt><dd><input type="checkbox" name="active" ' . ($hook->active ? 'checked ' : '') . '" /></dd>');
+	print('<dt><strong>Event</strong></dt><dd><input type="text" name="event" value="' . join(' ', $hook->events) . '" /></dd>');
+	print('<dt><strong>URL</strong></dt><dd><input type="text" name="url" size="80" value="' . $hook->config->url . '" /></dd>');
+	print('<dt><strong>Secret</strong></dt><dd><input type="text" name="secret" value="' . $hook->config->secret . '" /></dd>');
+	print('</dl>');
+	print('<input type="submit" value="' . ($id ? 'Update' : 'Add') . '" />');
+	print('</form>');
+}
+
+function patch_hook($id, $event, $url, $secret, $active) {
+	$body = json_encode(array(
+		'name' => 'web',
+		'config' => array(
+			'content_type' => 'form',
+			'url' => $url,
+			'secret' => $secret
+		),
+		'events' => array($event),
+		'active' => $active
+	));
+	send_request(HTTP_METH_PATCH, 'hooks/' . $id, $body);
+}
+
+function add_hook($event, $url, $secret, $active) {
+	$body = json_encode(array(
+		'name' => 'web',
+		'config' => array(
+			'content_type' => 'form',
+			'url' => $url,
+			'secret' => $secret
+		),
+		'events' => array($event),
+		'active' => $active
+	));
+	send_request(HTTP_METH_POST, 'hooks', $body);
+}
+
+function delete_hook($id) {
+	send_request(HTTP_METH_DELETE, 'hooks/' . $id, '');
+}
+
+function print_thing($thing) {
+	if (is_array($thing)) {
+		print('<ol start="0">');
+		foreach ($thing as $value) {
+			print('<li>');
+			print_thing($value);
+			print('</li>');
+		}
+		print('</ol>');
+	}
+	elseif (is_object($thing)) {
+		print('<dl>');
+		foreach ($thing as $key => $value) {
+			print('<dt><strong>' . $key . '</strong></dt><dd>');
+			print_thing($value);
+			print('</dd>');
+		}
+		print('</dl>');
+	}
+	else
+		print($thing);
+}
+
+if (!isset($_POST['action']))
+	$_POST['action'] = 'list';
+
+if ($_POST['action'] == 'edit')
+	edit_hook($_POST['id']);
+else if ($_POST['action'] == 'new')
+	edit_hook(false);
+else {
+	if ($_POST['action'] == 'add')
+		add_hook($_POST['event'], $_POST['url'], $_POST['secret'], isset($_POST['active']) && $_POST['active'] ? '1' : '0');
+	else if ($_POST['action'] == 'patch')
+		patch_hook($_POST['id'], $_POST['event'], $_POST['url'], $_POST['secret'], isset($_POST['active']) && $_POST['active'] ? '1' : '0');
+	else if ($_POST['action'] == 'delete')
+		delete_hook($_POST['id']);
+	list_hooks();
+}
+?>
---
+++ index.php
@@ -0,0 +1,112 @@
+<?
+/*
+ * This hook was registered using
+ *
+ * curl -i -u "user:password" https://api.github.com/hub \
+ * -Fhub.mode=subscribe \
+ * -Fhub.topic=https://github.com/USER/REPO/events/EVENT \
+ * -Fhub.callback=SOMEURL
+ *
+ * See https://github.com/github/github-services/issues/166
+ */
+require_once "Mail.php";
+
+function sendNotification($subject, $body) {
+	global $mail_from, $mail_host, $mail_username, $mail_password;
+	$from = 'GitHub <' . $mail_from . '>';
+	$reply_to = 'msysgit@googlegroups.com';
+	$to = 'Johannes Schindelin <johannes.schindelin@gmx.de>';
+
+	$headers = array ('From' => $from,
+		'To' => $to,
+		'Reply-to' => $reply_to,
+		'Subject' => $subject);
+	$smtp = Mail::factory('smtp',
+		array ('host' => $mail_host,
+			'auth' => true,
+			'username' => $mail_username,
+			'password' => $mail_password));
+
+	$mail = $smtp->send($to, $headers, $body);
+
+	if (PEAR::isError($mail)) {
+		echo("<p>" . $mail->getMessage() . "</p>");
+	} else {
+		echo("<p>Message successfully sent!</p>");
+	}
+}
+
+function tmpLog($msg) {
+	$date = strftime('%Y-%m-%d-%H:%M:%S');
+	$msg = $date . ' ' . $msg . "\n";
+	ob_start();
+	print("request headers\n");
+	var_dump(apache_request_headers());
+	print("GET\n");
+	var_dump($_GET);
+	print("POST\n");
+	var_dump($_POST);
+	print("SERVER\n");
+	var_dump($_SERVER);
+	if (isset($_POST['payload']))
+		var_dump(json_decode($_POST['payload']));
+	$msg .= ob_get_clean();
+	ob_end_flush();
+	$file = fopen('/tmp/github-notifier.log', 'a');
+	fwrite($file, $msg);
+	fclose($file);
+}
+
+$remote = $_SERVER['REMOTE_ADDR'];
+$headers = apache_request_headers();
+$event = isset($headers['X-Github-Event']) ? $headers['X-Github-Event'] : false;
+if (!event ||
+		!isset($_POST['payload']) ||
+		!preg_match('/\.github\.com$/', gethostbyaddr($remote))) {
+	include('hooks.php');
+}
+else {
+	$payload = json_decode($_POST['payload']);
+	$repository = $payload->repository;
+	$owner = $repository->owner->login;
+	if ($owner != 'msysgit' && $owner != 'dscho')
+		exit('Invalid owner: ' . $owner);
+	$subject = '[github] ';
+	$repo_name = $repository->owner->login . '/' . $repository->name;
+
+	if ($event == 'issues') {
+		$issue = $payload->issue;
+		$subject .= 'Issue ' . $issue->number . ' ' . $payload->action . ' (' . $issue->title . ')';
+		$body = 'See ' . $issue->html_url . "\n\n"
+			. $issue->title . "\n\n"
+			. $issue->body;
+	}
+	elseif ($event == 'issue_comment') {
+		$issue = $payload->issue;
+		$comment = $payload->comment;
+		$subject .= 'Comment ' . $payload->action . ' on issue ' . $issue->number . ' (' . $issue->title . ')';
+		$body = 'See ' . $issue->html_url . "\n\n"
+			. $payload->sender->login . " added/edited comment:\n\n"
+			. $comment->body;
+	}
+	elseif ($event == 'pull_request') {
+		$number = $payload->number;
+		$pull = $payload->pull_request;
+		$subject .= 'Pull request #' . $pull->number . ' ' . $pull->action . ' (' . $pull->title . ')';
+		$body = 'See ' . $pull->html_url . "\n\n"
+			. $pull->title . "\n\n"
+			. $pull->body;
+	}
+	elseif ($event == 'push') {
+		$ref = $payload->ref;
+		$subject .= 'Pushed ' . $payload->size . ' commit' . ($payload->size > 1 ? 's' : '') . ' to ' . $ref;
+		$body = 'See ' . $repository->html_url;
+	}
+	else
+		exit('invalid event: ' + $event);
+
+	include('mail-settings.php');
+	sendNotification($subject, $body);
+}
+
+?>
